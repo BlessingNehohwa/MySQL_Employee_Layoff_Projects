4b42d929b6d1a3b4946fe976397015f2fddd5b70
Blessing Nehohwa <82777016+BlessingNehohwa@users.noreply.github.com>
2026-01-04 21:50:53 +0000

    Add files via upload

---
+++ 2. Where statement,Like,Having,Order by,Groupby.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics
+WHERE first_name='Leslie';
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+WHERE age>40 and age<50;
+
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
